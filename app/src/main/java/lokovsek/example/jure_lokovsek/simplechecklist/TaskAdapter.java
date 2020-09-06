d71e71c5664073c6b76eb3efdb0c94a28ce4598c
JureLokovsek <jurext@hotmail.com>
2020-09-06 10:43:24 +0200

    Migrated to AndroidX

--- app/src/main/java/lokovsek/example/jure_lokovsek/simplechecklist/TaskAdapter.java
+++ app/src/main/java/lokovsek/example/jure_lokovsek/simplechecklist/TaskAdapter.java
@@ -1,6 +1,6 @@
 package lokovsek.example.jure_lokovsek.simplechecklist;
 
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
